4ca1ed033f21a8e3bb5d74745e8a47eefc7d8f7b
Norm Barnard <barnardn@gmail.com>
2018-02-18 13:01:00 -0500

    Initial Commit

---
+++ WeatherClock/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WeatherClock
+//
+//  Created by Norm Barnard on 2/18/18.
+//  Copyright © 2018 NormBarnard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
