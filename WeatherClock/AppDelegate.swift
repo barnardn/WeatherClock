4ce20b2fe2b367ec419262237ee9714a41afbd7a
Norm Barnard <barnardn@gmail.com>
2018-02-18 15:14:05 -0500

    initial commit

--- WeatherClock/AppDelegate.swift
+++ WeatherClock/AppDelegate.swift
@@ -11,17 +11,29 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    
     @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+
+            button.image = NSImage(named: NSImage.Name("icon-statusbar"))
+            button.action = #selector(printQuote(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    @objc func printQuote(_ sender: NSStatusBarButton) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
+    }
 
 }
 
