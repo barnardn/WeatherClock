ca4b3da581b07603e69b234a11e8786d7fd62b0a
Norm Barnard <barnardn@gmail.com>
2018-02-19 16:11:02 -0500

    network class stub

---
+++ WeatherClock/Network/OWMCurrentConditions.swift
@@ -0,0 +1,20 @@
+//
+//  OWMCurrentConditions.swift
+//  WeatherClock
+//
+//  Created by Norm Barnard on 2/19/18.
+//  Copyright © 2018 NormBarnard. All rights reserved.
+//
+
+import Foundation
+
+class OWMCurrentConditions {
+    
+    private let apiKey: String
+    
+    init(apiKey _apiKey: String) {
+        apiKey = _apiKey
+    }
+    
+    
+}
