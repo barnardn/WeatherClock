c8f132af0216a6d8e24568efd846d161724fb221
Norm Barnard <barnardn@gmail.com>
2018-03-05 08:16:31 -0500

    removed non-reactive network fetch

--- WeatherClock/Network/OWMCurrentConditions.swift
+++ WeatherClock/Network/OWMCurrentConditions.swift
@@ -9,12 +9,6 @@
 import Foundation
 import ReactiveSwift
 
-enum OWMFetchResult {
-    case success(weather: WeatherConditions)
-    case network(error: Error)
-    case badData(data: Data?)
-}
-
 enum OWMError: Error {
     case unknown
     case missingAppID
@@ -38,7 +32,6 @@ enum OWMFetchRequest {
         }
     }
     
-    /// puti in an extension
     private func apiKey() -> String? {
         guard let path = Bundle.main.path(forResource: "apikeys", ofType: "plist") else { fatalError() }
         guard let keysDict = NSDictionary.init(contentsOfFile: path) as? [String:String] else { fatalError() }
@@ -46,64 +39,28 @@ enum OWMFetchRequest {
     }
 }
 
-
-
-typealias WeatherResultsBlock = (OWMFetchResult) -> Void
-
 class OWMCurrentConditions {
     
-    enum Constants {
-        static let apiPath = "http://api.openweathermap.org/data/2.5/weather"
-        static let appidKey = "appid"
-        static let zip = "zip"
-        static let imperialUnits = "units=imperial"
-    }
-    
-    private let apiKey: String
     private let urlSession: URLSession
-    private var task: URLSessionDataTask?
     
-    init(apiKey _apiKey: String) {
-        apiKey = _apiKey
+    init() {
         let sessionConfiguration = URLSessionConfiguration.ephemeral
         sessionConfiguration.timeoutIntervalForResource = 30.0
         urlSession = URLSession(configuration: sessionConfiguration)
     }
-    
-    func fetch(forZipcode zipCode: String, handler: @escaping WeatherResultsBlock) {
-        let apiLink = "\(Constants.apiPath)?\(Constants.appidKey)=\(apiKey)&\(Constants.zip)=\(zipCode)&\(Constants.imperialUnits)"
-        guard let url = URL(string: apiLink) else { fatalError() }
-        let task = urlSession.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                handler(.network(error: error))
-                return
-            }
-            guard let data = data else {
-                handler(.badData(data: nil))
-                return
-            }
-            let decoder = JSONDecoder()
-            do {
-                let weather = try decoder.decode(WeatherConditions.self, from: data)
-                handler(.success(weather: weather))
-            } catch  {
-                handler(.badData(data: data))
-            }
-        }
-        task.resume()
-    }
 
     func fetch(weatherRequest: OWMFetchRequest) -> SignalProducer<WeatherConditions, OWMError> {
-        return dataProduder(forRequest: weatherRequest).flatMap(.concat, weatherProducer)
+        return dataProducer(forRequest: weatherRequest).flatMap(.concat, weatherProducer)
     }
     
-    private func dataProduder(forRequest request: OWMFetchRequest) -> SignalProducer<Data, OWMError> {
+    private func dataProducer(forRequest request: OWMFetchRequest) -> SignalProducer<Data, OWMError> {
         return SignalProducer{ observer, disposable in
             do {
                 let urlRequest = try request.toURLRequest()
                 let task = self.urlSession.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
                     if let error = error {
                         observer.send(error: .network(underlyingError: error))
+                        return
                     }
                     guard let _data = data else {
                         observer.send(error: .badOrMissingData(data: data))
